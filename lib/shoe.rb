70685f383dc9ab9128ddf43a91644d4e80536908
Kevin K <doilerjar@outlook.com>
2018-08-08 08:44:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,37 +1,11 @@
 # Make your shoe class here!
 class Shoe
   
-  def initialize(title)
-    @title = title 
+  def initialize(brand)
+    @brand = brand
   end
 
-  def title 
-    @title 
-  end 
-  
-  def author=(author)
-    @author = author 
-  end 
-  
-  def author 
-    @author 
-  end 
-  
-  def page_count=(num)
-    @page_count = num 
-  end 
-  
-  def page_count
-    @page_count
-  end 
-  
-  def genre=(genre)
-    @genre = genre
-  end 
-  
-  def genre 
-    @genre
-  end 
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
