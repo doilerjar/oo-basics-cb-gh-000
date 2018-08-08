8792abf8fb42d74bf991b05442b7d68c870c4df4
Kevin K <doilerjar@outlook.com>
2018-08-08 08:54:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,5 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
-  attr_accessor :brand, :color, :size, :material
-  attr_reader: :condition
   
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end 
 end
